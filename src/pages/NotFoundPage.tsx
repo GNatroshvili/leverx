79ccdccba13ba9013e5c1035478861b6b8047d69
GNatroshvili <gigi.natroshvili@gmail.com>
2025-12-11 15:32:40 +0400

    fix: fix back to main button not redirecting bug

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../../styles/scss/reset.scss';
-import '../../styles/scss/layout.scss';
-import '../../styles/scss/404-not-found.scss';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/scss/reset.scss";
+import "../../styles/scss/layout.scss";
+import "../../styles/scss/404-not-found.scss";
 
 const NotFoundPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="not-found-page">
-      <div className="not-found-content">
-        <img src="/assets/not-found.png" alt="404 not found" className="not-found-image" />
-        <h1 className="not-found-title">404</h1>
-        <p className="not-found-message">Page Not Found</p>
-        <button 
-          className="back-home-btn"
-          onClick={() => navigate('/main')}
+    <body>
+      <div className="not-found-wrapper">
+        <img
+          src="./assets/404.png"
+          alt="404-not-found-image"
+          className="image"
+        />
+        <p className="title">404 Page not found</p>
+        <p className="description">
+          sorry, we can't find the page you're looking for.
+        </p>
+        <a
+          href="/main"
+          className="back-to-main-btn clickable"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/main");
+          }}
         >
-          Go Back Home
-        </button>
+          Go to the home page
+        </a>
       </div>
-    </div>
+    </body>
   );
 };
 
