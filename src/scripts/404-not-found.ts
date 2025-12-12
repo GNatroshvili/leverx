2de13e65e4fbc68de56af5c1e3c34d4895b41a13
GNatroshvili <gigi.natroshvili@gmail.com>
2025-12-12 15:22:44 +0400

    fix: delete unused html, css and assests files

--- src/scripts/404-not-found.ts
+++
@@ -1,7 +0,0 @@
-// here importing SCSS styles for 404 page
-import '../../styles/scss/reset.scss';
-import '../../styles/scss/layout.scss';
-import '../../styles/scss/404-not-found.scss';
-
-// 404 page doesn't need any JavaScript logic
-console.log('404 page loaded');
